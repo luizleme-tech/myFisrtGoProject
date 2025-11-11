41bebfdaafccdf6ce250ab69d74c1fbe21fd7b9d
Luiz Leme <luizleme.tech@gmail.com>
2025-11-10 21:43:36 -0300

    definindo arrays

--- myFirstGoProject/main.go
+++ myFirstGoProject/main.go
@@ -1,62 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
-var idade int
-
-// bool
-// int int8 int16 int32 int64 (inteiros positivos e negativos)
-// uint uint8 uint16 uint32 uint64 uintptr (inteiros positivos)
-//
-// byte é a mesma coisa que uint8
-//
-// rune é a mesma coisa que int64
-//
-// float32 float64
-//
-// complex64 complex128
-//
-// string
-
-// conversaoDireta
 func main() {
-	var x int = 10084
-	f := float64(x)
-	s := string(x) //salva em uma rune e salva no primeiro caractere da string
-	sc := strconv.FormatInt(int64(x), 10)
-	const i int = 100
-	// somente rune, string, bool e valores numericos podem ser constantes
+	//arr := [3]int{1, 2, 3, 4}
+	//arr := [10]int{5: 400, 7: 300}
+	arr := [10]string{4: "hello, world"}
 
-	fmt.Println(f)
-	fmt.Println(s)
-	fmt.Println(sc)
+	fmt.Println(arr)
 }
-
-/*
-func main() {
-	//var x bool
-	var b uint8 = 10
-	takeByte(b)
-}
-
-func takeByte(b byte) {
-	fmt.Println(b)
-}
-
-func somar(a, b int) int {
-	return a + b
-}
-
-func varsStrings() {
-	//var nome, sobrenome string = "Pedro", "Pessoa"
-	// var (
-	// 	nome      = "Pedro"
-	// 	sobrenome = "Pessoa"
-	// )
-	nome := "Pedro"
-	sobrenome := "Pessoa"
-	fmt.Println(nome, sobrenome, idade)
-}*/
