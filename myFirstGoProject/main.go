26a75397156f39f49fe29df919b3928cbb387f4c
Luiz Leme <luizleme.tech@gmail.com>
2025-11-17 21:10:12 -0300

    estudo de loops

--- myFirstGoProject/main.go
+++ myFirstGoProject/main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	//arr := [3]int{1, 2, 3, 4}
-	//arr := [10]int{5: 400, 7: 300}
-	arr := [10]string{4: "hello, world"}
+	var res int
+	//for i := 0; i < 10; i++ {
+	//	res++
+	//}
+	var i int
+	//for ; i < 10; i++ {
+	//	res++
+	//}
+	for i < 10 {
+		res++
+		i++
+	}
+	fmt.Println(res)
 
-	fmt.Println(arr)
+	arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//for j, elem := range arr {
+	//	fmt.Println(j, elem)
+	//}
+
+	//blank identifier
+	for _, elem := range arr {
+		fmt.Println(elem)
+	}
+
+	for range 2 {
+		fmt.Println("dentro")
+	}
+
+	const n = 10
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for k := 0; k < n; k++ {
+		go func() {
+			defer wg.Done()
+			fmt.Println(k)
+		}()
+	}
+	wg.Wait()
 }
