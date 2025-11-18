a92624a19ec20c8de6024c9262d3daf106d54ad0
Luiz Leme <luizleme.tech@gmail.com>
2025-11-17 21:34:17 -0300

    estudo de switches

--- myFirstGoProject/main.go
+++ myFirstGoProject/main.go
@@ -3,20 +3,53 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
-func main() {
-	if 1 < 2 {
-		fmt.Println("sim")
-	}
+//func main() {
+//	do(1)
+//	do(2)
+//	do(3)
+//}
+//
+//func do(x int) {
+//	switch x {
+//	case 1:
+//		fmt.Println(1)
+//	case 2:
+//		fmt.Println(2)
+//	default:
+//		fmt.Println("outra coisa")
+//	}
+//}
 
-	//if x := math.Sqrt(4); x < 10 { //shot statement
-	if x := math.Sqrt(4); x < 1 { //shot statement
-		fmt.Println(x)
-		//} else if x > 0 {
-	} else if x < 1 {
-		fmt.Println(" maior que zero")
-	} else {
-		fmt.Println(" caiu no else")
+func main() {
+	switch x := math.Sqrt(4; x {
+	case 2:
+		fmt.Println(" resultado e 2")
+	default:
+		fmt.Println("algo deu errado")
 	}
+}
+
+//func isWeekend(x time.Time) bool {
+//	switch x.Weekday() {
+//	case time.Sunday, time.Saturday
+//		return true
+//	default:
+//		return false
+//	}
+//}
+
+func do( x any) {
+	switch t:= x.(type) {
+	case string:
+		takeString(t)
+	case int:
+	case nil:
+	}
+}
+
+func takeString(s string) {
+	fmt.Println(s)
 }
